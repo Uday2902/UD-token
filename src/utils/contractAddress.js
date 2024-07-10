0cb43898c5cfcf81dffe65a50ea404f80ba42605
Uday <udaykpatel2004@gmail.com>
2024-07-10 13:02:42 +0530

    production debugging

--- src/utils/contractAddress.js
+++ src/utils/contractAddress.js
@@ -1,2 +1,4 @@
 const contractAddress = `${process.env.CONTRACT_ADDRESS}`
+console.log("Contract address at reading directly -> ", process.env.CONTRACT_ADDRESS);
+console.log("Contract address at reading -> ", contractAddress);
 export default contractAddress;
